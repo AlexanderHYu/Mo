19076716c95ff84b2cbdfbef01426d46e9defc78
Alexander Yu <Alexander Yu>
2019-09-15 14:29:25 -0400

    experiments on hud

--- Mo/HomeScene.swift
+++ Mo/HomeScene.swift
@@ -11,4 +11,35 @@ import GameplayKit
 
 class HomeScene: SKScene {
     
+    let homeCamera = SKCameraNode(fileNamed: "cameraInHome")
+    let homeScene = SKScene(fileNamed: "HomeScene")
+    
+    override func sceneDidLoad() {
+        <#code#>
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = atPoint(location)
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
 }
